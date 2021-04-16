e80a4500d44d21e5d9420874aaf580cfcc1a9066
Nova_Max <tom.maier@abwesend.de>
2021-04-17 00:12:38 +0200

    support updating

--- Install/install.bat
+++ Install/install.bat
@@ -1,2 +1,4 @@
-xcopy /s %~dp0\XBatteryStatus.exe "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp"
+taskkill /IM "XBatteryStatus.exe" /F
+timeout /T 10 /nobreak
+xcopy /s %~dp0\XBatteryStatus.exe "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp" /Y
 start "" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\XBatteryStatus.exe"
