ae406008e2664ac84314d5bc12c40e731b1a036b
Nova_Max <tom.maier@abwesend.de>
2021-04-16 23:13:12 +0200

    added install scripts

---
+++ Install/install.bat
@@ -0,0 +1,2 @@
+xcopy /s %~dp0\XBatteryStatus.exe "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp"
+start "" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\XBatteryStatus.exe"
