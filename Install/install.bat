2ec8e2a6ea4fb4d814f4bd72a0c0300bf237e126
Nova_Max <tom.maier@abwesend.de>
2021-04-17 00:16:58 +0200

    remove debugging delay

--- Install/install.bat
+++ Install/install.bat
@@ -1,4 +1,4 @@
 taskkill /IM "XBatteryStatus.exe" /F
-timeout /T 10 /nobreak
+timeout /T 1 /nobreak
 xcopy /s %~dp0\XBatteryStatus.exe "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp" /Y
 start "" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\XBatteryStatus.exe"
