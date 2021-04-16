bff7230b02ad1813768d188a8aa1b71ca6c81320
Nova_Max <tom.maier@abwesend.de>
2021-04-16 23:09:21 +0200

    finished icon directory change

--- XBatteryStatus/Properties/Resources.Designer.cs
+++ XBatteryStatus/Properties/Resources.Designer.cs
@@ -71,16 +71,6 @@ namespace XBatteryStatus.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap icon001 {
-            get {
-                object obj = ResourceManager.GetObject("icon001", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
         /// </summary>
         internal static System.Drawing.Icon icon10 {
