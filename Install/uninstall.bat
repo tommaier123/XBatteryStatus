c826702d6bc6bb5d81c911be713d72a8f60bc528
Alexander CherryTea <Akiyamka@gmail.com>
2022-06-05 17:35:36 +0200

    closes #2

---
+++ Install/uninstall.bat
@@ -0,0 +1,18 @@
+@echo off
+goto check_Permissions
+
+:check_Permissions
+    echo Administrative permissions required. Detecting permissions...
+    
+    net session >nul 2>&1
+    if %errorLevel% == 0 (
+        echo Success: Administrative permissions confirmed.
+    ) else (
+        echo Failure: Current permissions inadequate.
+    )
+    
+    pause >nul
+
+taskkill /IM "XBatteryStatus.exe" /F
+timeout /T 1 /nobreak
+del "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\XBatteryStatus.exe" /Y
