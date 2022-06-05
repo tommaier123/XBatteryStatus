fd56d58eb380a21e5c308bf04843a1cbcfc6a59f
Alexander CherryTea <Akiyamka@gmail.com>
2022-06-05 17:34:15 +0200

    Update install.bat
    Add check for admin rights
    
    https://stackoverflow.com/questions/4051883/batch-script-how-to-check-for-admin-rights

--- Install/install.bat
+++ Install/install.bat
@@ -1,3 +1,18 @@
+@echo off
+goto check_Permissions
+
+:check_Permissions
+    echo Administrative permissions required. Detecting permissions...
+    
+    net session >nul 2>&1
+    if %errorLevel% == 0 (
+        echo Success: Administrative permissions confirmed.
+    ) else (
+        echo Failure: Current permissions inadequate.
+    )
+    
+    pause >nul
+
 taskkill /IM "XBatteryStatus.exe" /F
 timeout /T 1 /nobreak
 xcopy /s %~dp0\XBatteryStatus.exe "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp" /Y
